050d4af369a67806d4d06a06103fc4f549be0c80
DmitrySergeevich <efos.ds@gmail.com>
2022-06-27 19:16:02 +0700

    header-menu

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,3 +1,17 @@
-export function headerJS() {
+import $ from "jquery";
 
+export function headerJS() {
+    
+    // burger-menu
+    $('.header__burger, .header__overlay').click(function(){
+        $('.header__burger').toggleClass('open');
+        $('.header__overlay').toggleClass('open');
+        $('.nav-secondary').toggleClass('open');        
+    });
+
+    $('.nav-secondary__item a').click(function() {
+        $('.header__burger').toggleClass('open');
+        $('.header__overlay').toggleClass('open');
+        $('.nav-secondary').toggleClass('open'); 
+    })
 }
