39b89166e48548de0a9e46f93711aa6a965bb27d
ProFIT1337 <santiago.farias@yandex.ru>
2022-06-30 12:08:32 +0700

    add anim, when section opening

--- src/js/modules/about.js
+++ src/js/modules/about.js
@@ -18,64 +18,24 @@ export function aboutJS() {
     });
   });
 
-  //   const pinkCardDiv = document.querySelector('.about__card--pink');
-  //   const greenCardDiv = document.querySelector('.about__card--green');
-  //   const topPosition = 50;
-  //   const bottomPosition = -50;
-  //   const leftPosition = 70;
-  //   const rightPosition = -70;
-
-  //   let pinkCard = { card: pinkCardDiv, currPositionX: leftPosition, currPositionY: topPosition, isActive: true };
-  //   let greenCard = { card: greenCardDiv, currPositionX: rightPosition, currPositionY: bottomPosition, isActive: false };
-  //   const cards = [pinkCard, greenCard];
-
-  //   pinkCard.card.style.transform = `translate(${leftPosition}px, ${topPosition}px)`;
-  //   pinkCard.card.style.zIndex = 1;
-  //   greenCard.card.style.transform = `translate(${rightPosition}px, ${bottomPosition}px)`;
-  //   greenCard.card.style.zIndex = 0;
-
-  //   let activeCard = pinkCard;
-  //   let otherCard = greenCard;
-
-  //   let isAnimationActive = false;
-
-  //   function moveCardTo(card, isActive) {
-  //     if (isActive) {
-  //       card.card.style.transform = `translate(${x}px, ${y}px)`;
-  //     } else {
-  //     }
-  //   }
-
-  //   function moveCards() {
-  //     activeCard.card.style.transform = `translate(${leftPosition + 80}px, ${topPosition - 50}px)`;
-  //     otherCard.card.style.transform = `translate(${rightPosition - 80}px, ${bottomPosition + 50}px)`;
-  //   }
-
-  //   function animateCards() {
-  //     isAnimationActive = true;
-
-  //     moveCards();
-
-  //     isAnimationActive = false;
-  //   }
-
   let animationIsActive = false;
   let activeCard = 'pink';
+  const cards = document.querySelector('.about__cards');
 
   function activateAnimation() {
     if (!animationIsActive && activeCard == 'pink') {
       animationIsActive = true;
-      document.querySelector('.about__cards').classList.toggle('about__cards_animate');
+      cards.classList.add('about__cards_animate');
       document.querySelectorAll('.about__card').forEach((card) => {
-        card.classList.toggle('about__card_animate');
+        card.classList.add('about__card_animate');
       });
       setTimeout(() => {
-        document.querySelector('.about__cards').style.transform = 'rotateX(75deg) rotateZ(-180deg)';
+        cards.style.transform = 'rotateX(75deg) rotateZ(-180deg)';
         document.querySelectorAll('.about__card').forEach((card) => {
-          card.classList.toggle('about__card_animate');
+          card.classList.remove('about__card_animate');
           card.style.transform = 'rotateX(-90deg) rotateY(180deg) rotateZ(0deg)';
         });
-        document.querySelector('.about__cards').classList.toggle('about__cards_animate');
+        cards.classList.remove('about__cards_animate');
         animationIsActive = false;
         activeCard = 'green';
       }, 1000);
@@ -85,17 +45,17 @@ export function aboutJS() {
   function activateForwardAnimation() {
     if (!animationIsActive && activeCard == 'green') {
       animationIsActive = true;
-      document.querySelector('.about__cards').classList.toggle('about__cards_animate2');
+      cards.classList.add('about__cards_animate2');
       document.querySelectorAll('.about__card').forEach((card) => {
-        card.classList.toggle('about__card_animate2');
+        card.classList.add('about__card_animate2');
       });
       setTimeout(() => {
-        document.querySelector('.about__cards').style.transform = 'rotateX(75deg) rotateZ(0deg)';
+        cards.style.transform = 'rotateX(75deg) rotateZ(0deg)';
         document.querySelectorAll('.about__card').forEach((card) => {
-          card.classList.toggle('about__card_animate2');
+          card.classList.remove('about__card_animate2');
           card.style.transform = 'rotateX(-90deg) rotateY(0deg) rotateZ(0deg)';
         });
-        document.querySelector('.about__cards').classList.toggle('about__cards_animate2');
+        cards.classList.remove('about__cards_animate2');
         animationIsActive = false;
         activeCard = 'pink';
       }, 1000);
