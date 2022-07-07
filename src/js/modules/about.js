0939a5ae09473857db764299caa5caa22d7b2038
DmitrySergeevich <efos.ds@gmail.com>
2022-07-07 13:56:01 +0700

    временная ветка с отключенными блоками

--- src/js/modules/about.js
+++ src/js/modules/about.js
@@ -6,100 +6,97 @@ export function aboutJS() {
     const pinkBlock = $(".about__info--pink template").html();
     const greenBlock = $(".about__info--green template").html();
 
-    $(".about__info--pink").toggleClass("open", "switch");
-    $(".about__info--green").toggleClass("open", "switch");
-    $(this).toggleClass("about__info-toggle--green", "switch");
+    $(".about__info--pink").toggleClass("open");
+    $(".about__info--green").toggleClass("open");
+    $(this).toggleClass("about__info-toggle--green");
 
     $(this).text((idx, oldText) => {
-      if (oldText == pinkBlock) {
-        return greenBlock;
+      if (oldText == greenBlock) {
+        return pinkBlock;
       }
-      return pinkBlock;
+      return greenBlock;
     });
   });
 
-  let animationIsActive = false;
-  let activeCard = "pink";
-  const cards = document.querySelector(".about__cards");
+  //   let animationIsActive = false;
+  //   let activeCard = "pink";
+  //   const cards = document.querySelector(".about__cards");
 
-  function activateAnimation() {
-    let screenResolution = parseInt(window.innerWidth);
-    if (!animationIsActive && activeCard == "pink" && screenResolution > 1439) {
-      animationIsActive = true;
-      cards.classList.add("about__cards_animate");
-      document.querySelectorAll(".about__card").forEach((card) => {
-        card.classList.add("about__card_animate");
-      });
-      setTimeout(() => {
-        cards.style.transform = "rotateX(75deg) rotateZ(-180deg)";
-        document.querySelectorAll(".about__card").forEach((card) => {
-          card.classList.remove("about__card_animate");
-          card.style.transform =
-            "rotateX(-90deg) rotateY(180deg) rotateZ(0deg)";
-        });
-        cards.classList.remove("about__cards_animate");
-        animationIsActive = false;
-        activeCard = "green";
-      }, 1000);
-    }
-  }
+  //   function activateAnimation() {
+  //     let screenResolution = parseInt(window.innerWidth);
+  //     if (!animationIsActive && activeCard == "pink" && screenResolution > 1439) {
+  //       animationIsActive = true;
+  //       cards.classList.add("about__cards_animate");
+  //       document.querySelectorAll(".about__card").forEach((card) => {
+  //         card.classList.add("about__card_animate");
+  //       });
+  //       setTimeout(() => {
+  //         cards.style.transform = "rotateX(75deg) rotateZ(-180deg)";
+  //         document.querySelectorAll(".about__card").forEach((card) => {
+  //           card.classList.remove("about__card_animate");
+  //           card.style.transform =
+  //             "rotateX(-90deg) rotateY(180deg) rotateZ(0deg)";
+  //         });
+  //         cards.classList.remove("about__cards_animate");
+  //         animationIsActive = false;
+  //         activeCard = "green";
+  //       }, 1000);
+  //     }
+  //   }
 
-  function activateForwardAnimation() {
-    let screenResolution = parseInt(window.innerWidth);
-    if (
-      !animationIsActive &&
-      activeCard == "green" &&
-      screenResolution > 1439
-    ) {
-      animationIsActive = true;
-      cards.classList.add("about__cards_animate2");
-      document.querySelectorAll(".about__card").forEach((card) => {
-        card.classList.add("about__card_animate2");
-      });
-      setTimeout(() => {
-        cards.style.transform = "rotateX(75deg) rotateZ(0deg)";
-        document.querySelectorAll(".about__card").forEach((card) => {
-          card.classList.remove("about__card_animate2");
-          card.style.transform = "rotateX(-90deg) rotateY(0deg) rotateZ(0deg)";
-        });
-        cards.classList.remove("about__cards_animate2");
-        animationIsActive = false;
-        activeCard = "pink";
-      }, 1000);
-    }
-  }
+  //   function activateForwardAnimation() {
+  //     let screenResolution = parseInt(window.innerWidth);
+  //     if (
+  //       !animationIsActive &&
+  //       activeCard == "green" &&
+  //       screenResolution > 1439
+  //     ) {
+  //       animationIsActive = true;
+  //       cards.classList.add("about__cards_animate2");
+  //       document.querySelectorAll(".about__card").forEach((card) => {
+  //         card.classList.add("about__card_animate2");
+  //       });
+  //       setTimeout(() => {
+  //         cards.style.transform = "rotateX(75deg) rotateZ(0deg)";
+  //         document.querySelectorAll(".about__card").forEach((card) => {
+  //           card.classList.remove("about__card_animate2");
+  //           card.style.transform = "rotateX(-90deg) rotateY(0deg) rotateZ(0deg)";
+  //         });
+  //         cards.classList.remove("about__cards_animate2");
+  //         animationIsActive = false;
+  //         activeCard = "pink";
+  //       }, 1000);
+  //     }
+  //   }
 
-  document
-    .querySelector(".about__card.about__card--green")
-    .addEventListener("mousemove", activateAnimation);
-  document
-    .querySelector(".about__card.about__card--pink")
-    .addEventListener("mousemove", activateForwardAnimation);
+  //   document
+  //     .querySelector(".about__card.about__card--green")
+  //     .addEventListener("mousemove", activateAnimation);
+  //   document
+  //     .querySelector(".about__card.about__card--pink")
+  //     .addEventListener("mousemove", activateForwardAnimation);
+  // }
+
+  // let mouseMoved = false;
+  // if (
+  //   document.querySelector(".about__cards") &&
+  //   document.querySelector(".about__card-wrapper")
+  // ) {
+  //   document.querySelector(".about__cards").addEventListener("mousemove", () => {
+  //     mouseMoved = true;
+  //   });
+  //   document
+  //     .querySelector(".about__cards")
+  //     .addEventListener("mousedown", (event) => {
+  //       mouseMoved = false;
+  //     });
+  //   document
+  //     .querySelector(".about__cards")
+  //     .addEventListener("mouseup", (event) => {
+  //       if (event.target.tagName == "BUTTON" || mouseMoved) {
+  //         return;
+  //       }
+  //       let card = event.target.closest(".about__card-wrapper");
+  //       card.classList.toggle("flipped");
+  //     });
 }
-
-// function rotateCard() {
-let mouseMoved = false;
-if (
-  document.querySelector(".about__cards") &&
-  document.querySelector(".about__card-wrapper")
-) {
-  document.querySelector(".about__cards").addEventListener("mousemove", () => {
-    mouseMoved = true;
-  });
-  document
-    .querySelector(".about__cards")
-    .addEventListener("mousedown", (event) => {
-      mouseMoved = false;
-    });
-  document
-    .querySelector(".about__cards")
-    .addEventListener("mouseup", (event) => {
-      if (event.target.tagName == "BUTTON" || mouseMoved) {
-        return;
-      }
-      let card = event.target.closest(".about__card-wrapper");
-      card.classList.toggle("flipped");
-    });
-}
-
-// }
--- src/js/modules/supports.js
+++ src/js/modules/supports.js
@@ -1,31 +1,28 @@
 import $ from "jquery";
 
 export function tabs() {
-  $(function () {
-    const content = $(".supports__content > li");
-    content.hide().filter(":first").show();
-
-    // Клики по вкладкам.
-    $(".supports__tabs a")
-      .click(function () {
-        content.hide();
-        content.filter(this.hash).show();
-        $(".supports__tabs a").removeClass("open");
-        $(this).addClass("open");
-        return false;
-      })
-      .filter(":first")
-      .click();
-
-    // Клики по якорным ссылкам.
-    // $('.tabs-target').click(function(){
-    //     $('#tabs .tabs-nav a[href=' + $(this).attr('href')+ ']').click();
-    // });
-
-    // Отрытие вкладки из хеша URL
-    // if(window.location.hash){
-    //     $('#tabs-nav a[href=' + window.location.hash + ']').click();
-    //     window.scrollTo(0, $("#" . window.location.hash).offset().top);
-    // }
-  });
+  // $(function () {
+  //   const content = $(".supports__content > li");
+  //   content.hide().filter(":first").show();
+  //   // Клики по вкладкам.
+  //   $(".supports__tabs a")
+  //     .click(function () {
+  //       content.hide();
+  //       content.filter(this.hash).show();
+  //       $(".supports__tabs a").removeClass("open");
+  //       $(this).addClass("open");
+  //       return false;
+  //     })
+  //     .filter(":first")
+  //     .click();
+  // Клики по якорным ссылкам.
+  // $('.tabs-target').click(function(){
+  //     $('#tabs .tabs-nav a[href=' + $(this).attr('href')+ ']').click();
+  // });
+  // Отрытие вкладки из хеша URL
+  // if(window.location.hash){
+  //     $('#tabs-nav a[href=' + window.location.hash + ']').click();
+  //     window.scrollTo(0, $("#" . window.location.hash).offset().top);
+  // }
+  // });
 }
